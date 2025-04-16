de98212d4e81a2c7f92354da5edd546da6f0eeb1
t4rg3t <t4rg3t1337@gmail.com>
2025-04-16 23:12:04 +0300

    task4

---
+++ task4/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+//3. Число четное или нечетное
+//Напишите программу, которая проверяет, является ли введенное пользователем число четным или нечетным.
+
+func main() {
+	fmt.Println(isEven(4))
+	fmt.Println(isEven(5))
+}
+
+func isEven(n int) bool {
+	return n%2 == 0
+}
